daaf81dba2096176b27144c695d06d3b6e1aa470
Basak <chgy2131@naver.com>
2021-07-26 18:12:18 +0900

    Update MessageQueue_sendQ_example.py

--- sample/MessageQueue_sendQ_example.py
+++ sample/MessageQueue_sendQ_example.py
@@ -1,11 +1,11 @@
 import pika
 
-cred = pika.PlainCredentials('admin','1234')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='211.179.42.130',port=5672,credentials=cred)    )
+cred = pika.PlainCredentials('rabbit','MQ321') #MQTT계정 ID,PW를 차례로 입력
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='211.179.42.130',port=5672,credentials=cred)) #MQTT서버의 IP, Port를 입력
 
 channel = connection.channel()
-channel.queue_declare(queue='sensor')
+channel.queue_declare(queue='sensor') #사용할 토픽 입력
 
-channel.basic_publish(exchange='',  routing_key='sensor', body='fucking hot')
+channel.basic_publish(exchange='',  routing_key='sensor', body='message example') #exchange, routing_key, body(message)를 차례로 입력
  
 connection.close()
